dcb8eaaa68ecea045f03f300e67488e95001a6d5
locked in worker <work@10-23-0-31.dynapool.nyu.edu>
2025-03-03 20:42:03 -0500

    pedalboard page added

--- dobusywork.py
+++ dobusywork.py
@@ -1,9 +1,20 @@
-from os import listdir
-from os.path import isfile, join
+# from os import listdir
+# from os.path import isfile, join
 
-## https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+# ## https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
 
-mypath = "/Users/work/Desktop/NYU WORK/web/salvadorio.github.io/imgs/projectPreviewImg"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+# mypath = "/Users/work/Desktop/NYU WORK/web/salvadorio.github.io/imgs/projectPreviewImg"
+# onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-print(onlyfiles)
+# print(onlyfiles)
+
+
+
+string = "hello dylan"
+
+i = 0
+
+while string[i] != ' ':
+    i += 1
+   
+print(i)
